effaba4279ed70bfb39f8ffce48824e25b1d0c7f
Sanketvardhan <sanketvardhanbhonsle1@gmail.com>
2024-07-10 22:03:31 +0530

    Added data to database

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User')
 
-router.get('/',(req,res)=>{
-    obj={
-        jgk:'fjdk',
-        sk:89
-    }
-    res.json(obj)
+// Create a user using :POST "/api/auth/". Doesnt require Auth
+router.post('/',(req,res)=>{
+    console.log(req.body)
+    const user = User(req.body)
+    user.save()
+    res.send(req.body)
 })
 
 module.exports=router
